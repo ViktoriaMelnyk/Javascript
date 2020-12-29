6a10ce00ca2a51dd972cfbcc3c1b24ebe3313cce
ViktoriaMelnyk <oliviatvist12@gmail.com>
2020-12-29 15:56:36 +0100

    notekeep added modal, changed js functionality

--- notekeep/main.js
+++ notekeep/main.js
@@ -1,59 +1,134 @@
-const addBTn = document.getElementById('add');
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const noteForm = document.getElementById('note-form');
+const noteTitle = document.getElementById('note-title');
+const noteText = document.getElementById('note-text');
 const container = document.getElementById('notes__container');
-const notes = JSON.parse(localStorage.getItem('notes'));
+//const deleteIcon = document.getElementById('delete');
 
-console.log(notes);
-if(notes){
-    notes.forEach(note => addNewNote(note));
+let notes = [];
+
+//Show modal
+
+function showModal() {
+    modal.classList.add('show-modal');
+    noteTitle.focus();
 }
 
-addBTn.addEventListener('click', () => addNewNote('Hello'));
-
-function addNewNote(text = '') {
-    const note = document.createElement('div');
-    note.classList.add('item');
-    note.innerHTML = `
-        <div class="tools">
-            <div class="tools-left">
-                <i class="fas fa-thumbtack" id='pinnNote'></i>
-                <i class="fas fa-palette"></i>
-                <i class="fas fa-pen-nib" id="edit"></i>
+//modal event listener
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () =>
+    modal.classList.remove('show-modal')
+);
+window.addEventListener('click', (e) =>
+    e.target === modal ? modal.classList.remove('show-modal') : false
+);
+//Build notes DOM
+function buildNotes() {
+    //clear 
+    container.textContent = '';
+    //Build items
+    notes.forEach((note) => {
+        const { title, text, createDate} = note;
+        console.log(title, text, createDate);
+        //Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+        item.innerHTML = `
+            <div class="tools">
+                <div class="tools-left">
+                    <i class="fas fa-thumbtack" id='pinnNote'></i>
+                    <i class="fas fa-palette" id='colour'></i>
+                    <i class="fas fa-edit"></i>
+                </div>
+                <div class="date">${createDate.toLocaleString()}</div>
+                <i class="fas fa-times" id="delete" title="Usunąć notatkę"></i>
             </div>
-            <i class="fas fa-times" id="delete" title="Usunąć notatkę"></i>
-        </div>
-        <div class="main ${text ? '' : 'hidden'}"></div>
-        <textarea class = "${text ? 'hidden' : ''}"></textarea>
-    `;
-    const editNote = note.querySelector('#edit');
-    const deleteNote = note.querySelector('#delete');
-    const main =note.querySelector  ('.main');
-    const textArea = note.querySelector('textarea');
-
-    deleteNote.addEventListener('click', () =>{
-        note.remove();
-        updateLS();
+            <div class="title">${title}</div>
+            <textarea class="note-textarea" maxlenght=“10”>${text}</textarea>
+        `;
+        
+        container.appendChild(item);
     });
-    editNote.addEventListener('click', () => {
-        main.classList.toggle('hidden');
-        textArea.classList.toggle('hidden');  
-    });
+}
+// delete note
 
-
-    updateLS();
-    container.appendChild(note);
-    
+//fetch Notes
+function fetchNotes() {
+    //get notes from LS
+    if (localStorage.getItem('notatki')) {
+        notes = JSON.parse(localStorage.getItem('notatki'));
+    } else {
+        notes = [
+            {
+                title: 'To jest twój notatnik',
+                text:
+              ' Tu morzesz tworzyć notatki w różnych kolorach, najważniejsze przypinać, jeżeli zamkniesz stronę, to zawsze możesz wrócic i zobaczyć swoje notatki',
+                createDate: new Date(),
+            },
+        ];
+        localStorage.setItem('notatki', JSON.stringify(notes));
+    }
+    buildNotes();
+}
+//Handle data from form
+function storeNote(e) {
+    e.preventDefault();
+    const titleValue = noteTitle.value;
+    let textValue = noteText.value;
+    console.log(titleValue, textValue);
+    const note = {
+        title: titleValue,
+        text: textValue,
+        createDate: new Date(),
+        // colour: true
+    };
+    notes.push(note);
+    console.log(notes);
+    localStorage.setItem('notatki', JSON.stringify(notes));
+    fetchNotes();
+    noteForm.reset();
+    noteTitle.focus();
 }
 
-function updateLS() {
-    const notesText = document.querySelectorAll('textarea');
-    const notes =[];
-    notesText.forEach(note => notes.push(note.value));
-    localStorage.setItem('notes', JSON.stringify(notes));
-}
+// event listener
+noteForm.addEventListener('submit', storeNote);
 
+fetchNotes();
 
-// console.log(notesFromLocalStorage);
+// console.log(notes);
+// if(notes){
+//     notes.forEach(note => addNewNote(note));
+// }
 
+// addBTn.addEventListener('click', () => addNewNote());
+
+// function addNewNote() {
+//     const note = document.createElement('div');
+//     note.classList.add('item');
+//     note.innerHTML = `
+//         <div class="tools">
+//             <div class="tools-left">
+//                 <i class="fas fa-thumbtack" id='pinnNote'></i>
+//                 <i class="fas fa-palette" id='colour'></i>
+//             </div>
+//             <i class="fas fa-times" id="delete" title="Usunąć notatkę"></i>
+//         </div>
+//         <div class="colours"></div>
+
+//         <textarea class="note-textarea" maxlenght=“10”></textarea>
+//     `;
+//     const deleteNote = note.querySelector('#delete');
+//     deleteNote.addEventListener('click', () =>{
+//         note.remove();
+//         updateLS();
+//     });
+
+//     updateLS();
+//     container.appendChild(note);
+
+// }
 
 
 // // 1. zapisywanie notatki i tablicy notatek w localStorage
@@ -76,7 +151,7 @@ function updateLS() {
 // localStorage.setItem(lsKey, );
 
 // // 2. wczytywanie z localStorage
-// 
+//
 
 // const mappedNotes = notesFromLocalStorage.map((note) => {
 //   note.createDate = new Date(note.createDate);
@@ -136,4 +211,3 @@ function updateLS() {
 //   const content = document.querySelector("#noteContent").value;
 //   console.log(title, content);
 // }
-
